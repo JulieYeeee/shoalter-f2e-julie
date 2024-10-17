caab076df93437bbe96a069bf57d0d278256143b
julieyeeee <oopsyeh056@gmail.com>
2024-10-17 18:10:30 +0800

    chore: set the basic repo setting

---
+++ src/app/home/page.tsx
@@ -0,0 +1,13 @@
+
+import SearchBar from "@/components/SearchBar"
+import Recommendation from "@/components/Recommendation"
+import ResultList from "@/components/ResultList"
+function Home() {
+    return <>
+        <SearchBar />
+        <Recommendation />
+        <ResultList />
+    </>
+}
+
+export default Home
---
+++ src/components/ResultList.tsx
@@ -0,0 +1,9 @@
+import { List } from "antd";
+function ResultList() {
+    return <div>
+        <h1>Result List</h1>
+        <List />
+    </div>
+}
+
+export default ResultList
