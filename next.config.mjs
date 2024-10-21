c16bb1dfaf5e3b1db11d249a6dce0f31735cbdc0
julieyeeee <oopsyeh056@gmail.com>
2024-10-21 11:49:31 +0800

    build: build

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  output: 'export',
+  basePath: '/shoalter-f2e-julie',
+  assetPrefix: '/shoalter-f2e-julie/',
 }
 
 export default nextConfig
